36f30cbdaec0f256f6a87ccdc396e80f9c1ad1a1
Abel Fernandez <abelfergodevelop@gmail.com>
2019-02-16 16:24:36 +0100

    Clase 142

--- miscelaneos/src/app/components/usuario/usuario-nuevo.component.ts
+++ miscelaneos/src/app/components/usuario/usuario-nuevo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-nuevo',
@@ -11,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioNuevoComponent implements OnInit {
 
-  constructor() { }
+    constructor(private activatedRoute: ActivatedRoute) {
+        this.activatedRoute.parent.params.subscribe(
+            parametros => {
+                console.log('Ruta Hija');
+                console.log(parametros);
+            }
+        );
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() { }
 
 }
